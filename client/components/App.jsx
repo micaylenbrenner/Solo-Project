b3ea8c0fd73cac4c4a6cfd1c916b9a29ea642675
micaylenbrenner <micaylen.brenner@gmail.com>
2022-08-11 09:24:16 -0700

    react components started

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>howdy hey hey ho</h1>
-            </div>
-        )
-    }
+import LogSheet from './client/components/LogSheet';
+
+// class App extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>howdy hey hey ho</h1>
+//             </div>
+//         )
+//     }
+// }
+
+const App = props => {
+  return (
+    <div className="router">
+      <main>
+      </main>    
+    </div>
+  )
 }
+
 export default App;
---
+++ client/components/LogSheet.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Component } from 'react';
+
+class LogSheet extends Component {
+
+}
+
+export default LogSheet;
